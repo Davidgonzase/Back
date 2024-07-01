b5231f42a9246cedab87b0fc04f792489dd0eba5
Davidgonzase <Davidgonzase@gmail.com>
2024-07-01 19:15:16 +0200

    Cambio env

--- apirestej/main.ts
+++ apirestej/main.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
-const MONGO_KEY = Deno.env.get("MONGO_KEY");
-
-console.log(MONGO_KEY)
+const env = await load();
+const MONGO_KEY = env.MONGO_KEY || Deno.env.get("MONGO_KEY")
 
 if(!MONGO_KEY||MONGO_KEY==""){
   console.log("Missing MONGO_KEY")
@@ -12,6 +12,7 @@ if(!MONGO_KEY||MONGO_KEY==""){
 
 try {
   await mongoose.connect(MONGO_KEY)
+  console.log("Connected to Mongoose...")
 } catch (error) {
   console.log("Unable to connect to mongoose")
   console.log(error)
@@ -21,4 +22,4 @@ try {
 
 const app = express();
 
-app.listen(8000, ()=>{console.log("API/REST ")})
+app.listen(8000, ()=>{console.log("API/REST on port 8000")})
